4d0af1365d46a3a6b688a01a21e61bd419eadddf
liyun <hanyuntaocn@163.com>
2023-09-06 21:05:07 +0800

    2023.9.6 21:06

---
+++ REL_ert_rtw/REL_private.h
@@ -0,0 +1,51 @@
+/*
+ * File: REL_private.h
+ *
+ * Code generated for Simulink model 'REL'.
+ *
+ * Model version                  : 1.308
+ * Simulink Coder version         : 9.7 (R2022a) 13-Nov-2021
+ * C/C++ source code generated on : Wed Sep  6 14:09:23 2023
+ *
+ * Target selection: ert.tlc
+ * Embedded hardware selection: Intel->x86-64 (Windows64)
+ * Code generation objectives: Unspecified
+ * Validation result: Not run
+ */
+
+#ifndef RTW_HEADER_REL_private_h_
+#define RTW_HEADER_REL_private_h_
+#include "rtwtypes.h"
+#include "Rte_Type.h"
+#include "REL.h"
+
+extern void REL_FLDoorRatSts(Boolean rtu_SI_b_DoorOpen, uint8_T
+  *rty_SO_e_DoorRatSts);
+extern void REL_FLDoorRlsDriver_Init(boolean_T *rty_SO_b_MotorA, boolean_T
+  *rty_SO_b_MotorB, uint8_T *rty_SO_e_MotorMode, boolean_T *rty_SO_b_Error,
+  uint8_T *rty_SO_e_MotorPwm);
+extern void REL_FLDoorRlsDriver(boolean_T rtu_SO_b_DoorRlsReq, Boolean
+  rtu_SI_b_DoorAjar, Boolean rtu_SI_b_CinchHome, Boolean rtu_SI_b_ElecDoorCfg,
+  uint8_T rtu_SI_e_DoorRlsDelayTime, boolean_T *rty_SO_b_MotorA, boolean_T
+  *rty_SO_b_MotorB, uint8_T *rty_SO_e_MotorMode, boolean_T *rty_SO_b_Error,
+  uint8_T *rty_SO_e_MotorPwm, DW_FLDoorRlsDriver_REL_T *localDW);
+extern void REL_RLDoorRlsReq_Init(boolean_T *rty_SO_b_DoorRlsReq, boolean_T
+  *rty_SO_b_UnlockReq, uint8_T *rty_SO_e_DoorRlsDelayTime);
+extern void REL_RLDoorRlsReq(boolean_T rtu_SI_b_OFF, UInt8 rtu_SI_e_EspVehSpd,
+  Boolean rtu_SI_b_EspVehSpdVld, UInt8 rtu_SI_e_VcuGearPosn, UInt8
+  rtu_SI_e_DoorLockSts, uint8_T rtu_SI_e_DoorRatSts, boolean_T
+  rtu_SI_b_DoorInSwSts, boolean_T rtu_SI_b_DoorOutSwSts, boolean_T
+  rtu_SI_b_DoorHandSwSts, UInt8 rtu_SI_e_DoorHandPosSts, Boolean
+  rtu_SI_b_CentSingleDoorSwSts, Boolean rtu_SI_b_CentAllDoorSwSts, Boolean
+  rtu_SI_b_ChildLckSts, boolean_T rtu_SI_b_DoorLockSetSts, boolean_T
+  rtu_SI_b_DoorLockOpenReq, boolean_T *rty_SO_b_DoorRlsReq, boolean_T
+  *rty_SO_b_UnlockReq, uint8_T *rty_SO_e_DoorRlsDelayTime, DW_RLDoorRlsReq_REL_T
+  *localDW);
+
+#endif                                 /* RTW_HEADER_REL_private_h_ */
+
+/*
+ * File trailer for generated code.
+ *
+ * [EOF]
+ */
---
+++ REL_ert_rtw/REL_types.h
@@ -0,0 +1,31 @@
+/*
+ * File: REL_types.h
+ *
+ * Code generated for Simulink model 'REL'.
+ *
+ * Model version                  : 1.308
+ * Simulink Coder version         : 9.7 (R2022a) 13-Nov-2021
+ * C/C++ source code generated on : Wed Sep  6 14:09:23 2023
+ *
+ * Target selection: ert.tlc
+ * Embedded hardware selection: Intel->x86-64 (Windows64)
+ * Code generation objectives: Unspecified
+ * Validation result: Not run
+ */
+
+#ifndef RTW_HEADER_REL_types_h_
+#define RTW_HEADER_REL_types_h_
+#include "Rte_Type.h"
+
+/* Model Code Variants */
+
+/* Forward declaration for rtModel */
+typedef struct tag_RTM_REL_T RT_MODEL_REL_T;
+
+#endif                                 /* RTW_HEADER_REL_types_h_ */
+
+/*
+ * File trailer for generated code.
+ *
+ * [EOF]
+ */
