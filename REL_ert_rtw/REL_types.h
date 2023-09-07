50fcdb9ee8feb46dac462421ac5ac27a852dd3c0
liyun <hanyuntaocn@163.com>
2023-09-07 17:23:39 +0800

    2023.9.7 17:23

--- REL_ert_rtw/REL_types.h
+++ REL_ert_rtw/REL_types.h
@@ -3,9 +3,9 @@
  *
  * Code generated for Simulink model 'REL'.
  *
- * Model version                  : 1.6
+ * Model version                  : 1.9
  * Simulink Coder version         : 9.7 (R2022a) 13-Nov-2021
- * C/C++ source code generated on : Thu Sep  7 14:01:26 2023
+ * C/C++ source code generated on : Thu Sep  7 17:22:48 2023
  *
  * Target selection: ert.tlc
  * Embedded hardware selection: Intel->x86-64 (Windows64)
